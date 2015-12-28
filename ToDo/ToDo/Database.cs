9fc98ab3ef77a58e83c860a61ce15ac14cf1a451
steroidfactory@gmail.com <steroidfactory@gmail.com>
2015-12-28 14:48:01 -0600

    Working Select Function with time calc

---
+++ ToDo/ToDo/Database.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ToDo
+{
+    class Database
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        private string table;
+
+
+
+        public void initDB()
+        {
+            setDB();
+        }
+
+        //
+        //  Connect to Database
+        //
+        private void setDB()
+        {
+            server = "10.11.3.3";
+            database = "wipedrive";
+            uid = "steroid";
+            password = "andriyk";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";" + "Convert Zero Datetime=True;";
+            connection = new MySqlConnection(connectionString);
+            table= "diskoperationlog";
+
+        }
+
+        //Username - started
+        //StartTime
+        //EndTime
+        //CustomComputerId - barcode
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    //Console.WriteLine("Connect is to open");
+                    connection.Open();
+                    return true;
+                }
+                else if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    //Console.WriteLine("Connection is already open");
+                    return true;
+                }
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    return false;
+                }
+                return true;
+
+            }
+            catch
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                //Console.WriteLine("Error in open connection is : " + ex);
+                return false;
+
+            }
+        }
+
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            //Console.WriteLine("Closing connectiion");
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch
+            {
+                //Console.WriteLine("Error in close connection: " + ex);
+                //MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        //Select statement
+        public List<string>[] Select(string barcode)
+        {
+            string query = "SELECT * FROM " + table + " WHERE CustomComputerId=" + "'" +barcode + "'" + ";";
+            //int countNum = Count();
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            //for (int i = 0; i<=4; i++)
+            //{
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+            //}
+
+            //Open connection
+            if (OpenConnection() == true)
+            {
+
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                // MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                using (MySqlDataReader dataReader = cmd.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        list[0].Add(dataReader["Username"] + "");
+                        list[1].Add(dataReader["StartTime"] + "");
+                        list[2].Add(dataReader["EndTime"] + "");
+
+                        //Console.WriteLine(dataReader["Index_ID"].ToString() + dataReader["QT"] + dataReader["OrderNumber"]
+                        //+ dataReader["ID"] + dataReader["TrackingNumber"] + dataReader["TimeIn"] );
+                    }
+                    //close Data Reader
+                    dataReader.Close();
+
+                    //close Connection
+                    CloseConnection();
+                }
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+
+        //Count statement
+        public int Count(string barcode)
+        {
+            string query = "SELECT Count(*) FROM " + table + " WHERE CustomComputerId=" + "'" + barcode + "'" + ";";
+            int Count = -1;
+
+            //Open Connection
+            if (OpenConnection() == true)
+            {
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //ExecuteScalar will return one value
+                Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                //close Connection
+                CloseConnection();
+
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
+        }
+
+
+    }
+}
+
--- ToDo/ToDo/Form1.cs
+++ ToDo/ToDo/Form1.cs
@@ -7,15 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data;
 
 namespace ToDo
 {
     public partial class Form1 : Form
     {
+        Database dbWipedrive = new Database();
         public Form1()
         {
             InitializeComponent();
+            dbWipedrive.initDB();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkDB("5861292");
+        }
+        
+        private void checkDB(string barcode)
+        {
+            DateTime timeStart = new DateTime();
+            DateTime timeEnd = new DateTime();
+            if (dbWipedrive.Count(barcode) > 0)
+            {
+                Console.WriteLine("Started By: " + dbWipedrive.Select(barcode)[0][0]);
+                timeStart = DateTime.Parse(dbWipedrive.Select(barcode)[1][0]);
+                timeEnd = DateTime.Parse(dbWipedrive.Select(barcode)[2][0]);
+                if (timeEnd > timeStart)
+                {
+                    MessageBox.Show("Ended");
+                }
+                else
+                {
+                    MessageBox.Show("Wiping");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No result found");
+            }
         }
     }
 }
